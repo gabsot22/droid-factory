a23e6af41412a03b674347ab335c99de572d27a2
Gabe Soto <gab_sot22@hotmail.com>
2020-02-05 17:48:19 -0500

    Working...

---
+++ cis237-assignment3/DroidCollection.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cis237_assignment3
+{
+    class DroidCollection
+    {
+        // Makes an array to hold instances of droids in DroidCollection class
+        DroidCollection[] droids = new DroidCollection[100];
+        
+        droids[0] = new DroidCollection("d");
+
+    }
+}
--- cis237-assignment3/Program.cs
+++ cis237-assignment3/Program.cs
@@ -10,6 +10,39 @@ namespace cis237_assignment3
     {
         static void Main(string[] args)
         {
+            // Make a new instance of the User Interface class
+            UserInterface ui = new UserInterface();
+
+            DroidCollection droidCollection = new DroidCollection();
+
+       
+
+            // Get input from the user
+            int choice = ui.GetUserInput();
+
+            // While the choice selected is not 2, continue to do work
+            while (choice != 3)
+            {
+                switch (choice)
+                {
+                    case 1:
+                        // Print Droid List
+                        string allDroidsString = droidCollection.ToString();
+                        if (!String.IsNullOrWhiteSpace(allDroidsString))
+                        {
+                            // Display all of the items
+                            ui.DisplayAllItems(allDroidsString);
+                        }
+                        else
+                        {
+                            // Display error message for all items
+                            ui.DisplayAllItemsError();
+                        }
+                        break;
+                }
+                
+            }
+
         }
     }
 }
